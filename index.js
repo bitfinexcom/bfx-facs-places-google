9fc4ffe209be92d7ca96f9af970b18230f3d5678
ezeswci <wernicke.ezequiel@gmail.com>
2020-05-21 16:25:21 +1000

    Improve path, query and url

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class GooglePlaces extends Base {
 
   queryAutoComplete (args) {
     if (!(args && args.input)) throw new Error('Input is required')
-    const path = 'queryautocomplete/'
+    const path = 'queryautocomplete'
     const keys = [
       'input', 'sessiontoken', 'offset', 'origin', 'location', 'radius',
       'language', 'types', 'components', 'strictbounds'
@@ -32,7 +32,7 @@ class GooglePlaces extends Base {
 
   details (args) {
     if (!(args && args.place_id)) throw new Error('place_id is required')
-    const path = 'details/'
+    const path = 'details'
     const keys = [
       'place_id', 'language', 'region', 'sessiontoken', 'fields'
     ]
@@ -45,8 +45,8 @@ class GooglePlaces extends Base {
   _req (path, args) {
     const key = this.conf.google.key
     const output = 'json'
-    const query = '?' + new URLSearchParams({ ...args, key }).toString()
-    const url = this.authority + path + output + query
+    const query = new URLSearchParams({ ...args, key }).toString()
+    const url = this.authority + path + '/' + output + '?' + query
     return axios.get(url)
   }
 }
